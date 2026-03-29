8f4f7b301c58f2f544ce3a346410132da6b1c9bc
Michael Staib <michael@chillicream.com>
2026-03-29 08:41:46 -0700

    Update hive router

--- apollo-federation/gateways/hive-router/install.sh
+++ apollo-federation/gateways/hive-router/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-VERSION="v0.0.42"
+VERSION="v0.0.43"
 curl -o- https://raw.githubusercontent.com/graphql-hive/router/main/install.sh | sh -s "$VERSION"
 echo "$VERSION" > version.txt
